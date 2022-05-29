9e848da5a88289e35fdf1d1f948ad154d2f22105
traza-ext <traza-ext@carglass.fr>
2022-05-29 21:12:32 +0200

    handle exception

--- api/src/Controller/CalculController.php
+++ api/src/Controller/CalculController.php
@@ -6,6 +6,7 @@ use App\Manager\Calculator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CalculController extends AbstractController
@@ -15,14 +16,24 @@ class CalculController extends AbstractController
     {
 	    $input = '';
 
-		if($content = $request->getContent()) {
-			$input = json_decode($content, true);
-		}
+	    try {
 
-	    $result = $calculator->execute($input['input']);
+			if($content = $request->getContent()) {
+				$input = json_decode($content, true);
+			}
 
-        return $this->json([
-            'result' => $result,
-        ]);
+		    $result = $calculator->execute($input['input']);
+
+	        return $this->json([
+		        'result' => $result,
+	        ]);
+
+        } catch (\Throwable $e) {
+
+		    return $this->json([
+			    'error' => $e->getMessage(),
+			    Response::HTTP_INTERNAL_SERVER_ERROR
+		    ]);
+        }
     }
 }
