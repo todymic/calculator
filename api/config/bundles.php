8fecd885d1fa71b215c5c1389d4b1e6250a53efc
Razafindrabekoto <gtody.rabekoto@gmail.com>
2022-08-11 16:02:10 +0200

    install orm and security component

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -7,4 +7,7 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
