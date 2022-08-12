64b3ea8a1d3e64db4d5f171da69d96bd9225bd24
Razafindrabekoto <gtody.rabekoto@gmail.com>
2022-08-12 14:36:13 +0200

    [API] Json Response for HttpException

---
+++ api/src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if (!$exception instanceof HttpException) {
+            return;
+        }
+
+        $jsonResponse = new JsonResponse([
+            'error' => $exception->getMessage(),
+        ], Response::HTTP_UNAUTHORIZED);
+
+        $event->setResponse($jsonResponse);
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+}
