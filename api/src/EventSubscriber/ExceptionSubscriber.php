ad8bc286aa3d5180c0a4795a2c49f5f65b637956
Razafindrabekoto <gtody.rabekoto@gmail.com>
2022-08-13 10:58:55 +0200

    [API] Return JSON for all exceptions response.

--- api/src/EventSubscriber/ExceptionSubscriber.php
+++ api/src/EventSubscriber/ExceptionSubscriber.php
@@ -4,24 +4,21 @@ namespace App\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class ExceptionSubscriber implements EventSubscriberInterface
 {
+    /**
+     * Return JsonResponse for all exceptions.
+     */
     public function onKernelException(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
 
-        if (!$exception instanceof HttpException) {
-            return;
-        }
-
         $jsonResponse = new JsonResponse([
             'error' => $exception->getMessage(),
-        ], Response::HTTP_UNAUTHORIZED);
+        ]);
 
         $event->setResponse($jsonResponse);
     }
