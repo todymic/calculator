349bd789d13436ec51454499196b22552e554552
Razafindrabekoto <gtody.rabekoto@gmail.com>
2022-08-13 20:58:03 +0200

    [API] add a method to get current user without passing ID

--- api/src/Controller/UserController.php
+++ api/src/Controller/UserController.php
@@ -12,18 +12,25 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
-#[Route('/users', name: 'app_user', methods: 'GET')]
+#[Route('/users', name: 'app_user')]
 #[isGranted('IS_AUTHENTICATED_FULLY')]
 class UserController extends AbstractController
 {
     use ControllerTrait;
 
-    #[Get('/{id}', name: 'profil')]
-    public function profile(User $user, SerializerInterface $serializer): View
+    #[Get('/{id}', name: 'profil', requirements: ['id' => "\d+"])]
+    public function getProfile(User $user, SerializerInterface $serializer): View
     {
         $response = json_decode($serializer->serialize($user, 'json', [AbstractNormalizer::GROUPS => 'get_user']), true);
 
         return $this->view($response);
     }
 
+    #[Route('/me', name: 'app_me', methods: 'GET')]
+    public function getCurrentUser(SerializerInterface $serializer): View
+    {
+        $response = json_decode($serializer->serialize($this->getUser(), 'json', [AbstractNormalizer::GROUPS => 'get_user']), true);
+
+        return $this->view($response);
+    }
 }
