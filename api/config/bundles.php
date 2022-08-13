f8a33f59b3ebf127b74ecf06bb6b97c45cb67785
Razafindrabekoto <gtody.rabekoto@gmail.com>
2022-08-13 10:40:21 +0200

    [API] delete JMS Serializer

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -5,7 +5,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
-    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
