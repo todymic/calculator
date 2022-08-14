70f94783bd7cd34f46232e3d2f7805666367f899
Razafindrabekoto <gtody.rabekoto@gmail.com>
2022-08-14 21:46:55 +0200

    [API] Use Bearer Authentication for security

--- api/src/Security/TokenAuthenticator.php
+++ api/src/Security/TokenAuthenticator.php
@@ -2,14 +2,12 @@
 
 namespace App\Security;
 
-use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
@@ -19,15 +17,15 @@ class TokenAuthenticator extends AbstractAuthenticator
 {
     public function supports(Request $request): ?bool
     {
-        return $request->headers->has('X-AUTH-TOKEN');
+        return $request->headers->has('Authorization')
+            && str_starts_with($request->headers->get('Authorization'), 'Bearer ');
     }
 
     public function authenticate(Request $request): Passport
     {
-        $apiToken = $request->headers->get('X-AUTH-TOKEN');
-        if (null === $apiToken) {
-            throw new CustomUserMessageAuthenticationException('No API token provided');
-        }
+        $authorizationHeader = $request->headers->get('Authorization');
+
+        $apiToken = substr($authorizationHeader, 7);
 
         return new SelfValidatingPassport(new UserBadge($apiToken));
     }
