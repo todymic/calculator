be7d0b64141632e01da53148012957c3f87d3756
Razafindrabekoto <gtody.rabekoto@gmail.com>
2022-08-15 17:56:42 +0200

    [UI] create userContext with user service

---
+++ ui/src/auth/AuthProvider.tsx
@@ -0,0 +1,77 @@
+import {ReactNode, useState, useEffect, useMemo, useContext} from 'react';
+import AuthContext from './AuthContext'
+import {useLocation, useNavigate} from "react-router-dom";
+import {UserInterface} from "../types/User.interface";
+import AuthService, {ILoginForm, IRegisterForm} from "../services/Auth.service";
+
+
+export const AuthProvider = ({children}: { children: ReactNode }): JSX.Element => {
+
+    const [user, setUser] = useState<UserInterface|undefined>({} as UserInterface);
+    const [error, setError] = useState<any>();
+    const [loading, setLoading] = useState<boolean>(false);
+    const [loadingInitial, setLoadingInitial] = useState<boolean>(true);
+
+    const location = useLocation();
+
+    // If we change page, reset the error state.
+    useEffect(() => {
+        if (error) setError(null);
+    }, [location.pathname]);
+
+    useEffect(() => {
+        AuthService.getCurrentUser()
+            .then((user) => setUser(user))
+            .catch((_error) => {
+            })
+            .finally(() => setLoadingInitial(false));
+    }, []);
+
+
+    const login = async (payload: ILoginForm) => {
+        setLoading(true);
+
+        return AuthService.login(payload)
+            .then((user) => {
+                setUser(user);
+
+            })
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false));
+    }
+
+    const signUp = (data: IRegisterForm) => {
+        setLoading(true);
+
+        return AuthService.register(data)
+            .then((user) => {
+                setUser(user);
+            })
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false));
+    }
+
+    const logout = () => {
+        setUser({});
+    }
+
+    const memoedValue = useMemo(() => ({
+            user,
+            loading,
+            error,
+            login,
+            signUp,
+            logout,
+        }),
+        [user, loading, error]
+    );
+    return (
+        <AuthContext.Provider value={memoedValue}>
+            {!loadingInitial && children}
+        </AuthContext.Provider>
+    );
+};
+
+export default function useAuth() {
+    return useContext(AuthContext);
+}
